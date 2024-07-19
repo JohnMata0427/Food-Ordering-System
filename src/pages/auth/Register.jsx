60d52b47367bfbfbdc91ebc19859b01ceed337d5
JohnMata0427 <john.mata@epn.edu.ec>
2024-07-19 14:15:52 -0500

    Register page

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,18 +1,57 @@
 // Importación de imágenes
 import chefsImg from "@assets/ui/chefs.png";
 import usuarioIcon from "@assets/icons/usuario.svg";
+import telefonoIcon from "@assets/icons/telefono.svg";
 import correoIcon from "@assets/icons/correo.svg";
 import contraseñaIcon from "@assets/icons/contraseña.svg";
 import enviarIcon from "@assets/icons/enviar.svg";
 
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Alerta from "@components/Alerta";
+import axios from "axios";
 
 export default function Register() {
-
     useEffect(() => {
         document.title = "Asociación de la ESFOT - Registro del Chef";
     }, []);
+
+    const [form, setForm] = useState({
+        nombre: "",
+        apellido: "",
+        telefono: "",
+        email: "",
+        password: "",
+    });
+
+    const [message, setMessage] = useState({});
+
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios
+            .post(`${import.meta.env.VITE_BACKEND_URL}/registro`, form)
+            .then((response) => {
+                console.log(response);
+                setMessage({
+                    mensaje: 'Usuario registrado correctamente, revise su correo electrónico para activar su cuenta',
+                    exito: true,
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+                setMessage({
+                    mensaje: error.response.data.msg,
+                    exito: false,
+                });
+            });
+    };
 
     return (
         <>
@@ -26,17 +65,22 @@ export default function Register() {
 
                 <form
                     className="flex flex-col gap-y-8 items-center sm:w-[65%] w-3/4"
+                    onSubmit={handleSubmit}
                 >
                     <div className="flex flex-col gap-y-4 w-full">
                         <div className="relative">
                             <img
                                 className="absolute top-[9px] left-3 size-4"
                                 src={usuarioIcon}
-                                alt=""
+                                alt="Icono de usuario"
                             />
                             <input
-                                className="border border-black placeholder-slate-700 text-md p-1 rounded-xl pl-9 w-full hover:border-slate-800 transition-all shadow-md shadow-black/20"
+                                id="nombre"
                                 type="text"
+                                name="nombre"
+                                value={form.nombre || ""}
+                                onChange={handleChange}
+                                className="border border-black placeholder-slate-700 text-md p-1 rounded-xl pl-9 w-full hover:border-slate-800 transition-all shadow-md shadow-black/20"
                                 placeholder="Ingrese su nombre"
                                 required
                             />
@@ -45,12 +89,52 @@ export default function Register() {
                         <div className="relative">
                             <img
                                 className="absolute top-[9px] left-3 size-4"
-                                src={correoIcon}
-                                alt=""
+                                src={usuarioIcon}
+                                alt="Icono de usuario"
                             />
                             <input
+                                id="apellido"
+                                type="text"
+                                name="apellido"
+                                value={form.apellido || ""}
+                                onChange={handleChange}
                                 className="border border-black placeholder-slate-700 text-md p-1 rounded-xl pl-9 w-full hover:border-slate-800 transition-all shadow-md shadow-black/20"
+                                placeholder="Ingrese su apellido"
+                                required
+                            />
+                        </div>
+
+                        <div className="relative">
+                            <img
+                                className="absolute top-[9px] left-3 size-4"
+                                src={telefonoIcon}
+                                alt="Icono de telefono"
+                            />
+                            <input
+                                id="telefono"
+                                type="tel"
+                                name="telefono"
+                                value={form.telefono || ""}
+                                onChange={handleChange}
+                                className="border border-black placeholder-slate-700 text-md p-1 rounded-xl pl-9 w-full hover:border-slate-800 transition-all shadow-md shadow-black/20"
+                                placeholder="Ingrese su número de teléfono"
+                                required
+                            />
+                        </div>
+
+                        <div className="relative">
+                            <img
+                                className="absolute top-[9px] left-3 size-4"
+                                src={correoIcon}
+                                alt="Icono de correo"
+                            />
+                            <input
+                                id="email"
+                                name="email"
+                                value={form.email || ""}
+                                onChange={handleChange}
                                 type="email"
+                                className="border border-black placeholder-slate-700 text-md p-1 rounded-xl pl-9 w-full hover:border-slate-800 transition-all shadow-md shadow-black/20"
                                 placeholder="Ingrese su correo electrónico"
                                 required
                             />
@@ -60,11 +144,15 @@ export default function Register() {
                             <img
                                 className="absolute top-[9px] left-3 size-4"
                                 src={contraseñaIcon}
-                                alt=""
+                                alt="Icono de contraseña"
                             />
                             <input
-                                className="border border-black placeholder-slate-700 text-md p-1 rounded-xl pl-9 w-full hover:border-slate-800 transition-all shadow-md shadow-black/20"
+                                id="password"
+                                name="password"
+                                value={form.password || ""}
+                                onChange={handleChange}
                                 type="password"
+                                className="border border-black placeholder-slate-700 text-md p-1 rounded-xl pl-9 w-full hover:border-slate-800 transition-all shadow-md shadow-black/20"
                                 placeholder="Ingrese una contraseña"
                                 required
                             />
@@ -74,16 +162,25 @@ export default function Register() {
                             <img
                                 className="absolute top-[9px] left-3 size-4"
                                 src={contraseñaIcon}
-                                alt=""
+                                alt="Icono de contraseña"
                             />
                             <input
-                                className="border border-black placeholder-slate-700 text-md p-1 rounded-xl pl-9 w-full hover:border-slate-800 transition-all shadow-md shadow-black/20"
+                                id="confirmarpassword"
+                                name="confirmarpassword"
+                                value={form.confirmarpassword || ""}
+                                onChange={handleChange}
                                 type="password"
+                                className="border border-black placeholder-slate-700 text-md p-1 rounded-xl pl-9 w-full hover:border-slate-800 transition-all shadow-md shadow-black/20"
                                 placeholder="Repita su contraseña"
                                 required
                             />
                         </div>
                     </div>
+
+                    {
+                        message.mensaje && <Alerta exito={message.exito}>{message.mensaje}</Alerta>
+
+                    }
 
                     <div className="flex flex-col items-center gap-y-2">
                         <button className="bg-gradient-to-r from-primary to-primary-dark py-1 px-16 flex items-center text-lg gap-x-2 rounded-xl hover:scale-105 transition-all shadow-md shadow-black/30">
@@ -95,10 +192,24 @@ export default function Register() {
                             />
                         </button>
                         <span className="text-sm ">
-                            ¿Tienes una cuenta? <Link className="font-bold text-secondary hover:text-secondary/80 transition-all" to="/login"> Inicia sesión</Link>
+                            ¿Tienes una cuenta?{" "}
+                            <Link
+                                className="font-bold text-secondary hover:text-secondary/80 transition-all"
+                                to="/login"
+                            >
+                                {" "}
+                                Inicia sesión
+                            </Link>
                         </span>
                         <span className="text-sm ">
-                            ¿Olvidaste tu contraseña? <Link className="font-bold text-secondary hover:text-secondary/80 transition-all" to="/login"> Recuperala</Link>
+                            ¿Olvidaste tu contraseña?{" "}
+                            <Link
+                                className="font-bold text-secondary hover:text-secondary/80 transition-all"
+                                to="/login"
+                            >
+                                {" "}
+                                Recuperala
+                            </Link>
                         </span>
                     </div>
                 </form>
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@layouts': path.resolve(__dirname, './src/layouts'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@assets': path.resolve(__dirname, './src/assets'),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@pages": path.resolve(__dirname, "./src/pages"),
+            "@layouts": path.resolve(__dirname, "./src/layouts"),
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@assets": path.resolve(__dirname, "./src/assets"),
+        },
     },
-  },
-})
+    server: {
+        proxy: {
+            "/api": {
+                target: "https://food-ordering-api-restful.onrender.com",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
+    },
+});
