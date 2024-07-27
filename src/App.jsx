a241e03db5d11394b52d1408ee24cb4d4376024d
Alan Rios <alan.rios.hd@gmail.com>
2024-07-27 15:47:28 -0500

    create ConfirmEmail, emailconfimed and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Register from "@pages/auth/Register";
-import Recovery from "./pages/auth/Recovery";
-import Verification from "./pages/auth/Verification";
-import Reset from "./pages/auth/Reset";
+// import Recovery from "./pages/auth/Recovery";
+// import Verification from "./pages/auth/Verification";
+//import Reset from "./pages/auth/Reset";
 import NotFound from "@pages/404";
 import Home from "@pages/Home";
 
 import { AuthProvider } from "@contexts/AuthProvider";
-import Reset from "./pages/auth/Verification";
-import Verification from "./pages/auth/Verification";
+//import Reset from "./pages/auth/Verification";
+//	import Verification from "./pages/auth/Verification";
+import Login from "./pages/auth/Login";
+import AuthLayout from "./layouts/AuthLayout";
+import ConfirmEmail from "./pages/auth/ConfirmEmail";
+import EmailConfirmed from "./pages/auth/EmailConfirmed";
 
 function App() {
 
@@ -22,9 +26,13 @@ function App() {
 						<Route path="/" element={isAuth ? <Home /> : <Navigate to="/auth/registro" />} />
 						<Route path="auth" element={<AuthLayout />}>
 							<Route path="registro" element={<Register />} />
-							<Route path="recuperar-contraseña" element={<Recovery />} /> 
-							<Route path="codigo-verificacion" element={<Verification/>} />
-							<Route path="restablecer-contraseña" element={<Reset/>} />
+							<Route path="confirmar-email" element={<ConfirmEmail />} />
+							<Route path="email-confirmado" element={<EmailConfirmed />} />
+							<Route path="iniciar-sesion" element={<Login />} />
+
+							{/* <Route path="recuperar-contraseña" element={<Recovery />} />
+							<Route path="codigo-verificacion" element={<Verification />} />
+							<Route path="restablecer-contraseña" element={<Reset />} /> */}
 						</Route>
 
 						<Route path="/home" element={<Home />} />
